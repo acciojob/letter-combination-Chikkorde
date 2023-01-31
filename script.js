1e270ca4ef164f47fda668446916cc927b9129c8
Chikkorde <120329211+Chikkorde@users.noreply.github.com>
2023-01-31 21:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function letterCombinations(input_digit) {
+
   //Complete the function
+
+	const mapping = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+
+function letterCombinations(digits) {
+    if (!digits) return [];
+
+    const res = [];
+    const dfs = (curr, index) => {
+        if (index === digits.length) {
+            res.push(curr);
+            return;
+        }
+
+        for (const char of mapping[digits[index] - '0']) {
+            dfs(curr + char, index + 1);
+        }
+    };
+
+    dfs('', 0);
+    return res;
+}
+
 }
 
 module.exports = letterCombinations;
